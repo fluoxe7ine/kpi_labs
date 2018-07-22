770276c32d338d0f4c69eed82edfd41af0154d53
fluoxe7ine <locbkolocb@gmail.com>
2018-07-22 15:51:44 +0300

    minor fixed to Baklan and Greed lab

--- 1-course/theory_of_algorithms/Greed/ip71_kozyriev_07.py
+++ 1-course/theory_of_algorithms/Greed/ip71_kozyriev_07.py
@@ -6,7 +6,7 @@ fin, fout = open('input.txt', 'r'), open(' ip71_kozyriev_07_output.txt', 'w')
 n = int(fin.readline())
 matrix = []
 
-data = [[int(line.rstrip().split(' ')[0]), int(line.rstrip().split(' ')[1])] for line in fin.readlines()[1:]]
+data = [[int(line.rstrip().split(' ')[0]), int(line.rstrip().split(' ')[1])] for line in fin][1:]
 
 for i in data:
     row = []
