86fc9801b3109e9f3f4d2a25537561c6cadc43b3
fluoxe7ine <locbkolocb@gmail.com>
2018-09-16 02:53:42 +0300

    added caesar encryption

--- 2-course/oop/lab_1/hard_task.js
+++ 2-course/oop/lab_1/hard_task.js
@@ -8,17 +8,25 @@ const rl = require('readline-sync');
 
 class Crypto {
     constructor() {
-        let _t = rl.question('Type your message (or press Enter to default): ') || 'Lorem ipsum dolor sit!';
-        this.getT = () => _t;
-        this.setT = (value) => _t = value;
+        let _t = rl.question('Type your message (or press Enter to default): ') || 'Lorem ipsum dolor sit';
+        this._getT = () => _t;
+        this._setT = (value) => _t = value;
     }
 
     get t() {
-        return this.getT();
+        return this._getT();
     }
 
+    encryptCaeser(n){
+        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        let text = this._getT().toLowerCase().split(' ');
+        let encryptedText = text.map(word => word.split('').map(letter => alphabet[(alphabet.indexOf(letter) + n) % alphabet.length]).join('')).join(' ');
+        this._setT(encryptedText);
+    }
 
 }
 
 let c1 = new Crypto();
+console.log(c1.t);
+c1.encryptCaeser(6);
 console.log(c1.t);
