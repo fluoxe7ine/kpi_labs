174acda5453332ed6bba67650dc88acafcea8b2e
fluoxe7ine <locbkolocb@gmail.com>
2018-09-16 14:51:01 +0300

    add caesar cipher and decipher implementation

--- 2-course/oop/lab_1/hard_task.js
+++ 2-course/oop/lab_1/hard_task.js
@@ -8,25 +8,35 @@ const rl = require('readline-sync');
 
 class Crypto {
     constructor() {
-        let _t = rl.question('Type your message (or press Enter to default): ') || 'Lorem ipsum dolor sit';
-        this._getT = () => _t;
-        this._setT = (value) => _t = value;
-    }
-
-    get t() {
-        return this._getT();
-    }
-
-    encryptCaeser(n){
+        let _t = rl.question('Type your message (or press Enter to default): ') || 'lorem ipsum dolor sit';
         let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        let text = this._getT().toLowerCase().split(' ');
-        let encryptedText = text.map(word => word.split('').map(letter => alphabet[(alphabet.indexOf(letter) + n) % alphabet.length]).join('')).join(' ');
-        this._setT(encryptedText);
-    }
 
+        this.encryptCaesar = (n) => {
+            let text = _t.toLowerCase().split(' ');
+            let encryptedText = text
+                .map(word => word.split('')
+                    .map(letter => alphabet[(alphabet.indexOf(letter) + n) % alphabet.length])
+                    .join(''))
+                .join(' ');
+            return encryptedText;
+        };
+
+        this.decryptCaesar = (encrMsg) => {
+            let n = 0;
+            while (encrMsg != _t) {
+                encrMsg = encrMsg.split(' ')
+                    .map(word => word.split('')
+                        .map(letter => alphabet.indexOf(letter) - 1 >= 0 ? alphabet[alphabet.indexOf(letter) - 1] : alphabet[alphabet.length + alphabet.indexOf(letter) -1]
+                        ).join('')
+                    ).join(' ');
+                ++n;
+            }
+            return {encrMsg, n};
+        };
+    }
 }
 
 let c1 = new Crypto();
-console.log(c1.t);
-c1.encryptCaeser(6);
-console.log(c1.t);
+let test = c1.encryptCaesar(4);
+console.log(test);
+console.log(c1.decryptCaesar(test));
