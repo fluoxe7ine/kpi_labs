a0d45d564f8b89439ad179122001e2938c8a1770
fluoxe7ine <locbkolocb@gmail.com>
2018-09-16 15:13:57 +0300

    added ROT1 implementation

--- 2-course/oop/lab_1/hard_task.js
+++ 2-course/oop/lab_1/hard_task.js
@@ -11,8 +11,20 @@ class Crypto {
         let _t = rl.question('Type your message (or press Enter to default): ') || 'lorem ipsum dolor sit';
         let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+        this.encryptRot1 = () => {
+            return this.encryptCaesar(1);
+        };
+
+        this.decryptRot1 = (encrMsg) => {
+            return encrMsg.split(' ')
+                .map(word => word.split('')
+                    .map(letter => alphabet.indexOf(letter) - 1 >= 0 ? alphabet[alphabet.indexOf(letter) - 1] : alphabet[alphabet.length + alphabet.indexOf(letter) -1]
+                    ).join('')
+                ).join(' ');
+        };
+
         this.encryptCaesar = (n) => {
-            let text = _t.toLowerCase().split(' ');
+            let text = _t.split(' ');
             let encryptedText = text
                 .map(word => word.split('')
                     .map(letter => alphabet[(alphabet.indexOf(letter) + n) % alphabet.length])
@@ -33,10 +45,14 @@ class Crypto {
             }
             return {encrMsg, n};
         };
+
     }
 }
 
-let c1 = new Crypto();
-let test = c1.encryptCaesar(4);
+let crypto = new Crypto();
+let test = crypto.encryptCaesar(1);
 console.log(test);
-console.log(c1.decryptCaesar(test));
+// console.log(c1.decryptCaesar(test));
+let test2 = crypto.encryptRot1();
+console.log(test2);
+console.log(crypto.decryptRot1(test2));
