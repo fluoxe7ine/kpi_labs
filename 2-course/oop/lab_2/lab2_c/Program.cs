68a9cdeb922fd5811d4b11311a0c319fa72206eb
fluoxe7ine <locbkolocb@gmail.com>
2018-10-03 00:55:18 +0300

    started work at lab_c

---
+++ 2-course/oop/lab_2/lab2_c/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab2_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bignum_arithmetic ba1 = new Bignum_arithmetic("-128");
+            Bignum_arithmetic ba2 = new Bignum_arithmetic("125");
+            Bignum_arithmetic ba3 = ba1 + ba2;
+            Console.WriteLine(ba3.bignum);
+        }
+    }
+}
