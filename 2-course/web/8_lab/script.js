7f4e8d6fa701cd25608682e6714a952fba64a8d0
fluoxe7ine <locbkolocb@gmail.com>
2018-10-14 18:06:32 +0300

    finished 8 lab web

--- 2-course/web/8_lab/script.js
+++ 2-course/web/8_lab/script.js
@@ -104,13 +104,73 @@ let makeMove = (function move() {
 })();
 
 //task 7
-//TODO:
+function calculateCourse() {
+    
+    let courses = {
+        math: 1000,
+        javascript: 1500,
+        cplusplus: 8000
+    };
+
+    let durations = {
+        half: 1,
+        hour: 1.25,
+        two: 1.45
+    };
+
+    let languages = {
+        en: 1,
+        ukr: 1.15,
+        ru: 1.15
+    };
+
+    let course = document.getElementById('t7-course').value;
+    let duration = document.getElementById('t7-duration').value;
+    let language = document.getElementById('t7-language').value;
+    let result = document.getElementById('res-7');
+    result.innerHTML = courses[course] * durations[duration] * languages[language];
+}
 
 //task 8
-//TODO:
+function changeTdBg(id) {
+    let td = document.querySelector('#t8').childNodes[1].querySelectorAll('td');
+    let color = prompt('Input color', '');
+    td[id].style.background = color;
+}
+
+function changeTableBg() {
+    let table = document.querySelector('#t8');
+    let color = prompt('Input color', '');
+    table.style.background = color;
+}
 
 //task 9
-//TODO:
+
+function calculatePrice() {
+    
+    let prices = {
+        milk: 10,
+        wine: 13,
+        beer: 8
+    };
+
+    let first = document.getElementById('t9-1').value,
+        second = document.getElementById('t9-2').value,
+        third = document.getElementById('t9-3').value;
+
+    let first_count = document.getElementById('t9-count-1').value,
+        second_count = document.getElementById('t9-count-2').value,
+        third_count = document.getElementById('t9-count-3').value;
+
+    let result = document.getElementById('res-9');
+    
+    // console.
+    result.innerHTML = 
+                        first_count * prices[first] + 
+                        second_count * prices[second] + 
+                        third_count * prices[third];
+
+}
 
 //task 10
 
@@ -134,11 +194,38 @@ function dividersCount() {
 }
 
 //task 12
-// TODO:
+let sum = function(num){ // функция для получения суммы цифр
+    let arr = num.toString().split(''),
+        s = 0;
+    arr.forEach((value) => s += parseInt(value));
+    return s;
+};
+
+let luckyTickets = (function(len){
+    let lenMiddle = len / 2,
+        maxSize = Math.pow(10, lenMiddle),
+        result = 0;
+
+    for (let i = 0; i < maxSize; i++)
+        for (let j = 0; j < maxSize; j++)
+            if (sum(i) == sum(j)) result++;
+    
+    let res = document.getElementById('t12');
+    res.innerHTML = result;
+})(6);
+
 
 //task 13
-// TODO:
 
+function calculateWeek(){
+    let date = new Date(document.getElementById('t13').value);
+    let diff = date - new Date(date.getFullYear(), 0, 0);
+    let oneDay = 1000 * 60 * 60 * 24;
+    let day = Math.floor(diff / oneDay);
+    let week = Math.floor(((day - 1) / 7) + 1);
+    let res = document.getElementById('res-13');
+    res.innerHTML = week;
+}
 
 //task 14
 function wordCounter() {
@@ -157,3 +244,12 @@ function wordCounter() {
         index % 2 == 0 ? elem.classList.add('rotate') : elem.classList.add('backrotate');
     });
 })();
+
+//task 16
+
+function validateEmail() {
+    let email = document.getElementById('t16').value;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let res = document.getElementById('res-16');
+    res.innerHTML = re.test(String(email).toLowerCase());
+}
