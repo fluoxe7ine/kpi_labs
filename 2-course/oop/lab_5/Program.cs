106876099dd57b11ee49209f6c613442704d0244
fluoxe7ine <locbkolocb@gmail.com>
2018-11-04 02:57:06 +0200

    added 5 lab oop

--- 2-course/oop/lab_5/Program.cs
+++ 2-course/oop/lab_5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lab_5
 {
@@ -6,7 +7,29 @@ namespace lab_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+
+            int n = Int32.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++) {
+                list.Add(Int32.Parse(Console.ReadLine()));
+            }
+            showList(list);
+            list.Sort();
+            showList(list);
+
+            int newElem = Int32.Parse(Console.ReadLine());
+            list.Add(newElem);
+            list.Sort();
+            Console.WriteLine("index: " + list.BinarySearch(newElem));
+            showList(list);
+        }
+
+        public static void showList(List<int> l) {
+            for (int i = 0; i < l.Count; i++) {
+                Console.Write(l[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
