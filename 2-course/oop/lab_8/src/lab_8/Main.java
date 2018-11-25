a0a123bc20932b1b4d1511cd3f834c830056fe5b
fluoxe7ine <locbkolocb@gmail.com>
2018-11-26 01:40:11 +0200

    added lab_8 oop

--- 2-course/oop/lab_8/src/lab_8/Main.java
+++ 2-course/oop/lab_8/src/lab_8/Main.java
@@ -4,9 +4,30 @@ import lab_8.Text;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Made by Kozyriev Oleksandr, IP-71");
-		System.out.println("Hello world!");
 		
+		//manual input
 		Text man = new Text();
-		Text auto = new Text("lorem ipsum dolor sit amet");
+		man.setKey();
+		man.setSymbol();
+		System.out.println("Key: " + man.k);
+		System.out.println("Symbol: " + man.symbol);
+		System.out.println("Text before formatting: ");
+		man.showText();
+		man.formatText();
+		System.out.println("Text after formatting: ");
+		man.showText();
+		
+		//auto input
+		Text auto = new Text("lorem ipsum dolor sit amet.\n lorem ipsum dolor sit amet.\n lorem ipsum dolor sit amet.");
+		auto.setKey(2);
+		auto.setSymbol("!");
+		System.out.println("Key: " + auto.k);
+		System.out.println("Symbol: " + auto.symbol);
+		System.out.println("Text before formatting: ");
+		auto.showText();
+		auto.formatText();
+		System.out.println("Text after formatting: ");
+		auto.showText();
+		
 	}
 }
--- 2-course/oop/lab_8/src/lab_8/Text.java
+++ 2-course/oop/lab_8/src/lab_8/Text.java
@@ -1,4 +1,5 @@
 package lab_8;
+import java.util.Scanner;
 
 public class Text {
 	public String data;
@@ -6,20 +7,63 @@ public class Text {
 	public int k;
 	
 	public Text() {
-		System.out.println("You choose manual input!");
+		System.out.println("Enter your text: ");
+		Scanner in = new Scanner(System.in);
+		String text = in.nextLine();
+		data = text;
 	}
 	
 	public Text(String text) {
 		this.data = text;
-		System.out.println("You choose string literal input");
 	}
 	
-	public void setKey(int k) {
-		this.k = k;
+	public void setKey(int key) {
+		this.k = key;
 	}
 	
 	public void setKey() {
+		System.out.println("Enter key: ");
+		Scanner in =  new Scanner(System.in);
+		int key = in.nextInt();
+		this.k = key;
+	}
+	
+	public void setSymbol(String s) {
+		this.symbol = s;
+	}
+	
+	public void setSymbol() {
+		System.out.println("Enter symbol: ");
+		Scanner in = new Scanner(System.in);
+		this.symbol = in.nextLine();
+	}
+	
+	public void showText() {
+		System.out.println(data);
+	}
+	
+	public void formatText() {
+		String[] wordsArray = data.split(" ");
+		String newText = "";
+		for (int i = 0; i < wordsArray.length; i++) {
+//			if (wordsArray[i].length() >= k) {
+				newText += replaceWord(wordsArray[i]) + " ";
+//			}
+		}
+		data = newText;
 		
 	}
 	
+	public String replaceWord(String word) {
+		String newWord = "";
+		int count = 1;
+		if (word.length() < k) return word;
+		for (int i = 0; i < word.length(); i++) {
+			if (count % k == 0 && word.toCharArray()[i] != '.' && word.toCharArray()[i] != '\n') {
+				newWord += symbol;
+			} else newWord += word.toCharArray()[i];
+			count++;
+		}
+		return newWord;
+	}
 }
